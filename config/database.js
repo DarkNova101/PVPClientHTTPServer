b880dd3618ed3e8ba59aefe35a0f8696cd86b774
Nolan <nehall.neh@gmail.com>
2020-03-07 08:17:48 -0600

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 // config/database.js
 module.exports = {
     connection: {
-        host: '138.68.3.36',
-        user: 'dark',
-        password: 'gNol4725!'
+        host: '1asdfasdfasdfasdfasfas6',
+        user: 'asdfasdfasdfasdf',
+        password: 'dfasdfsadfasdfsa'
     },
     database: 'Client'
 };
